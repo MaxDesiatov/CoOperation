065aacf9848e7d497b860934ee1941a19a0533fd
Max Desiatov <max.desiatov@gmail.com>
2016-01-10 17:42:28 +0000

    Initial Commit

---
+++ CoOperation/CoOperation.h
@@ -0,0 +1,19 @@
+//
+//  CoOperation.h
+//  CoOperation
+//
+//  Created by Max Desiatov on 10/01/2016.
+//  Copyright © 2016 Max Desiatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoOperation.
+FOUNDATION_EXPORT double CoOperationVersionNumber;
+
+//! Project version string for CoOperation.
+FOUNDATION_EXPORT const unsigned char CoOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoOperation/PublicHeader.h>
+
+
